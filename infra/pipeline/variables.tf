c11f6a889312a07e45b691ddf64034ee5467d7c0
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-10 16:32:22 +0100

    fix variable input problem

--- infra/pipeline/variables.tf
+++ infra/pipeline/variables.tf
@@ -1,10 +1,1 @@
-variable "project_name" {
-  description = "Name of the GCP Project"
-  type        = string
-}
 
-variable "test_var" {
-  description = "dummy variable to test ci pipeline"
-  type        = string
-  default     = "dummy"
-}

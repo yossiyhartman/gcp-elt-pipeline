45f154d1ce72b1e07d22e20d0d7ca1112dfca3d4
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-11 17:43:02 +0100

    add shell

--- src/gcp_pipeline/upload_to_bucket.py
+++ src/gcp_pipeline/upload_to_bucket.py
@@ -3,12 +3,14 @@ from datetime import datetime
 from google.cloud import storage
 
 if __name__ == "__main__":
-    storage_client = storage.Client()
-
     # get bucket
+    storage_client = storage.Client()
     bucket = storage_client.bucket("yossi-data-bucket")
 
+    # blobname
     time_of_upload = datetime.now().strftime("%d-%m-%Y")
-    object_name = time_of_upload + "/extraction-01/data.csv"
+    object_name = f"{time_of_upload}/data.csv"
     blob = bucket.blob(object_name)
+
+    # upload
     blob.upload_from_filename("./data/dummy_data.csv", content_type="csv")

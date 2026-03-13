b1a891e7f636e6290c59a35e7a5819415af48009
Yossi Hartman <yossiyhartman@gmail.com>
2026-03-13 16:42:00 +0100

    fix

--- infra/pipeline/modules/artifact_registry/main.tf
+++ infra/pipeline/modules/artifact_registry/main.tf
@@ -1,10 +1,8 @@
-data "google_artifact_registry_locations" "available" {}
-
 resource "google_artifact_registry_repository" "artifact-repo" {
   repository_id = var.artifact_repo_id
   description   = ""
   format        = "DOCKER"
-  location      = data.google_artifact_registry_locations.available.locations[0]
+  location      = "europe-west4"
 
 
 }
